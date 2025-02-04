033205717e951e1e6bcee0c90afcbc5f6cc56127
Ashparshp <ashparsh.connects@gmail.com>
2025-02-04 09:44:31 +0530

    refactor HTTP handlers and add new routes for Home and About pages

--- WebGo/01_Hello_World/main.go
+++ WebGo/01_Hello_World/main.go
@@ -5,16 +5,37 @@ import (
 	"net/http"
 )
 
-func main() {
-	println("Hello, World!")
+// HelloWorld is a simple HTTP handler function that writes a response to the client
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "Hello World")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Bytes written: %d\n", n)
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Bytes written: %d", n))
-	})
+// HomePage is a simple HTTP handler function that writes a response to the client
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home Page...")
+}
+
+// AboutPage is a simple HTTP handler function that writes a response to the client
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	addtwo := addValues(2, 2)
+	fmt.Fprintf(w, "This is the About Page... and 2 + 2 is: %d", addtwo)
+}
+
+// addValues is a simple function that adds two integers and returns the sum
+func addValues(x, y int) int {
+	sum := x + y
+	return sum
+}
+
+// main is the entry point for the application
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/home", HomePage)
+	http.HandleFunc("/about", AboutPage)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
